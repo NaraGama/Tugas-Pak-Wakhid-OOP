7dff8c96704f981124f4b606689af570575f1428
NaraGama <yakuzasama12@gmail.com>
2024-06-06 16:48:02 +0700

    lah kok gitu

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/Demas', function () {
+    return view('coba');
+});
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
